eb9118ec54c0c6b717837917ee253b3de1f5962d
Ellie Kemp <elliekemp95@gmail.com>
2021-04-06 19:35:56 +0100

    started on cruise ship GUI - added ports

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
+const Port = require("./src/port.js");
 const Ship = require("./src/ship.js");
+const Itinerary = require("./src/itinerary.js");
 
-const ship = new Ship("Southampton");
+const dover = new Port("Dover");
+const plymouth = new Port("Plymouth");
+console.log(dover);
+console.log(plymouth);
+const ports = [dover, plymouth];
+const itinerary = new Itinerary(ports);
+const explorer = new Ship(itinerary);
 
-const nextPort = new Port("Dover");
+console.log(explorer.currentPort);
+console.log(explorer.previousPort);
+explorer.setSail();
+explorer.dock();
 
-console.log(ship.startingPort);
-console.log(nextPort.name);
-
-ship.setSail();
-
-console.log(ship.startingPort);
+console.log(explorer.currentPort);
+console.log(explorer.previousPort);
--- src/port.js
+++ src/port.js
@@ -1,15 +1,20 @@
-class Port {
-  constructor(portName) {
-    this.name = portName;
-    this.ships = [];
+(function exportPort() {
+  class Port {
+    constructor(portName) {
+      this.name = portName;
+      this.ships = [];
+    }
+    addShip(ship) {
+      this.ships.push(ship);
+    }
+    removeShip(ship) {
+      const shipIndex = this.ships.indexOf(ship);
+      this.ships.splice(shipIndex, 1);
+    }
   }
-  addShip(ship) {
-    this.ships.push(ship);
+  if (typeof module !== "undefined" && module.exports) {
+    module.exports = Port;
+  } else {
+    window.Port = Port;
   }
-  removeShip(ship) {
-    const shipIndex = this.ships.indexOf(ship);
-    this.ships.splice(shipIndex, 1);
-  }
-}
-
-module.exports = Port;
+})();
